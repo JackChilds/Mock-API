c22806a16e5ed53eb508b2dde1219a81ee212845
Jack Childs <35969199+JackChilds@users.noreply.github.com>
2022-01-22 15:13:21 +0000

    Created Mock-API endpoint

--- api/[mock-api-0]/[mock-api-1].js
+++ api/[mock-api-0]/[mock-api-1].js
@@ -1,3 +1,11 @@
+/*
+*
+* Mock API
+* MIT License
+* By Jack Childs 2022
+*
+*/
+
 import { getResponseFromConfig } from "../../src/getResponseFromConfig";
 
 export default function createResponse(req, res) {
